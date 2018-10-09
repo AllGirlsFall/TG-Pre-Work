ef93a821e0ccd2f760c68e9ab6e3962bcf42601e
Just Joey <joey.maes@gmail.com>
2018-10-09 14:16:47 -0600

    Higher-order functions in JavaScript

---
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Higher-Order-Functions/Introduction.js
@@ -0,0 +1,3 @@
+/* Higher-order functions are functions that accept other functions as
+arguments and/or return functions as output This allows us to write more
+modular code which easier to both read and debug. */
